b1e38d9889f4d088890fd2a22855f58f9b23a633
Kelvin Hunter <kelvin@kleegnik.net>
2017-03-27 18:44:18 +0100

    Check hex input for hex2ieee.py

--- hex2ieee.py
+++ hex2ieee.py
@@ -4,6 +4,8 @@ from struct import unpack
 from sys import argv
 
 if len(argv)==2:
-	h = argv[1]
-	print unpack('!f', h.decode('hex'))[0]
-
+    h = argv[1]
+    if len(h)!=8:
+        print "Must be 8 hex nibbles"
+    else:
+        print unpack('!f', h.decode('hex'))[0]
